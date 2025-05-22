fb4a56e638f61813d7a98926aa092076d36aefa9
michelle <michellemuchoki8@gmail.com>
2025-05-22 14:08:37 +0300

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return_evens =[] #define the empty list of even numbers
+    for number in num_list:
+        if number %2 ==0: # checks if number is even
+            return_evens.append(number) # if even stores it
+    return return_evens # retums a list of even numbers
+
+   
+
+
+        
+
+
+
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
+    
+    
